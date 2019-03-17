11d14a1f6aadaf23a61949e520e7bffc9f16bc45
unknown <gaob974525@163.com>
2019-03-17 13:04:44 +0800

    加入redis缓存版

---
+++ babytun-redis/src/main/java/com/itlaoqi/babytun/BabytunApplication.java
@@ -0,0 +1,30 @@
+package com.itlaoqi.babytun;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+/**
+
+ */
+@SpringBootApplication
+@MapperScan("com.itlaoqi.babytun") //mybatis在SpringBoot启动的时候自动扫描mybatis实现的接口
+@EnableCaching //开启声明式缓存，利用注解来控制缓存的读写
+public class BabytunApplication {
+	@Bean
+	public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+		RedisTemplate redisTemplate = new RedisTemplate();
+		redisTemplate.setConnectionFactory(redisConnectionFactory);
+		redisTemplate.setKeySerializer(new StringRedisSerializer());
+		redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<Object>(Object.class));
+		return redisTemplate;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(BabytunApplication.class, args);
+	}
+}
---
+++ babytun-redis/src/main/java/com/itlaoqi/babytun/entity/GoodsDetail.java
@@ -0,0 +1,42 @@
+package com.itlaoqi.babytun.entity;
+
+import java.io.Serializable;
+
+public class GoodsDetail  implements Serializable {
+    private Long gdId;
+    private Long goodsId;
+    private String gdPicUrl;
+    private Integer gdOrder;
+
+    public Long getGdId() {
+        return gdId;
+    }
+
+    public void setGdId(Long gdId) {
+        this.gdId = gdId;
+    }
+
+    public Long getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Long goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getGdPicUrl() {
+        return gdPicUrl;
+    }
+
+    public void setGdPicUrl(String gdPicUrl) {
+        this.gdPicUrl = gdPicUrl;
+    }
+
+    public Integer getGdOrder() {
+        return gdOrder;
+    }
+
+    public void setGdOrder(Integer gdOrder) {
+        this.gdOrder = gdOrder;
+    }
+}
